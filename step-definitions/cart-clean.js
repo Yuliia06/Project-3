2d15003399e6ca7ea659ff5f7677c21695088edf
Yuliia <ydenysenko06@gmail.com>
2022-01-05 15:21:02 +0100

    Create cart-clean.js

---
+++ step-definitions/cart-clean.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+
+  this.Given(/^that I have two different items in the shopping cart$/, async function () {
+
+  });
+
+  this.When(/^I click reset cart button$/, async function () {
+
+  });
+
+  this.When(/^I confirm reseting the cart in opened dialog$/, async function () {
+
+  });
+
+  this.Then(/^the cart should not contain items$/, async function () {
+
+  });
+
+}
