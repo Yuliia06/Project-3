04c0cfff88499997aad85c86735d8f76d5e9db61
Yuliia <ydenysenko06@gmail.com>
2022-01-05 15:14:38 +0100

    Create cart-change-quantity.js
    !!!

---
+++ step-definitions/cart-change-quantity.js
@@ -0,0 +1,23 @@
+module.exports = function () {
+
+  this.Given(/^that I have an item in the shopping cart$/, async function () {
+
+  });
+
+  this.Given(/^that I have opened cart$/, async function () {
+
+  });
+
+  this.When(/^I click on increase quantity button of the item in the cart$/, async function () {
+
+  });
+
+  this.Then(/^the cart should show the correct total quantity of products$/, async function () {
+
+  });
+
+  this.Then(/^the cart should show correct total price$/, async function () {
+
+  });
+
+}
