a6e654f77a608ee7eb920e061843be87bf9e58a1
Yuliia <ydenysenko06@gmail.com>
2022-01-07 13:24:24 +0100

    Create cart-remove-item.js

---
+++ step-definitions/cart-remove-item.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+
+  this.Given(/^that I have an any item in the shopping cart$/, async function () {
+
+  });
+
+  this.When(/^I click decrease quantity button in the cart until quantity not 0$/, async function () {
+
+  });
+
+  this.Then(/^the cart should not contain item$/, async function () {
+
+  });
+
+}
